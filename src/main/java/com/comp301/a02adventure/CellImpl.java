790d83fd4a111d533ab47519f0b463fe755da9c5
Nick Nguyen <nickn@ad.unc.edu>
2024-09-21 18:44:26 -0400

    push

---
+++ src/main/java/com/comp301/a02adventure/CellImpl.java
@@ -0,0 +1,61 @@
+package com.comp301.a02adventure;
+
+public class CellImpl implements Cell {
+    private PositionImpl position;
+    private String name;
+    private String description;
+    private Inventory chest;
+    private boolean visited;
+    public CellImpl (int x, int y, String name, String description) {
+        this.position = new PositionImpl(x,y);
+        if (name == null || description == null) {
+            throw new IllegalArgumentException("Something is null");
+        }
+        this.name = name;
+        this.description = description;
+        this.visited = false;
+    }
+
+    public CellImpl(int x, int y) {
+        this(x, y, "", "");
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Position getPosition() {
+        return this.position;
+    }
+
+    public Inventory getChest() {
+        return chest;
+    }
+
+    public boolean getIsVisited() {
+        return visited;
+    }
+
+    public boolean hasChest() {
+        return chest != null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setChest(Inventory chest) {
+        this.chest = chest;
+    }
+
+    public void visit() {
+        this.visited = true;
+    }
+}
